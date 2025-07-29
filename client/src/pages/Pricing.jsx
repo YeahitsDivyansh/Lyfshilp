7f325ef32332f157a469a62f7d33ad768de5018b
YeahitsDivyansh <divyansh972003@gmail.com>
2025-07-29 14:28:05 +0530

    Fix margin adjustment in pricing section

--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -113,7 +113,7 @@ const Pricing = () => {
       </div>
 
       {/* Pricing Cards */}
-      <section className="relative z-10 bg-transparent -mt-40 px-4 pt-16 pb-20">
+      <section className="relative z-10 bg-transparent -mt-41 px-4 pt-16 pb-20">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {plans.map((plan, idx) => (
             <div
